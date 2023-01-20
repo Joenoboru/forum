e2e71322e063062c3092298311a94230ed1f3596
Noboru <joe4580803@gmail.com>
2023-01-20 11:56:22 +0800

    [Feature] Using .env and docker-compose

--- main.go
+++ main.go
@@ -2,31 +2,34 @@ package main
 
 import (
 	"fmt"
-
 	"forum/model"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
-const (
-	UserName string = "root"
-	Password string = ""
-	Addr     string = "127.0.0.1"
-	Port     int    = 3306
-	Database string = "goDB"
-)
-
 func init() {
 	// 設定資料庫連線
 	var err error
+
+	enverr := godotenv.Load()
+	if enverr != nil {
+		panic("Error loading .env file")
+	}
+	UserName := os.Getenv("DB_USER")
+	Password := os.Getenv("DB_PASSWORD")
+	Host := os.Getenv("DB_HOST")
+	Database := os.Getenv("DB_NAME")
+	Port := os.Getenv("DB_PORT")
 	//組合sql連線字串
-	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", UserName, Password, Addr, Port, Database)
-	db, err = gorm.Open(mysql.Open(addr))
+	addr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", UserName, Password, Host, Port, Database)
+	db, err = gorm.Open(mysql.Open(addr), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
